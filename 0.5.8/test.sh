cebbbda116e7d71d1485143c2be09c664ad4b757
icaoberg <icaoberg@andrew.cmu.edu>
2023-10-20 21:20:07 -0400

    Updated test

--- 0.5.8/test.sh
+++ 0.5.8/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 -c 'import random; [print(random.normalvariate(5, 5)) for _ in range(100000)]' | singularity exec singularity-lowcharts-0.5.8.sif lowcharts hist
+for i in {1..100}; do echo $((RANDOM % 1000)); done | singularity exec singularity-lowcharts-0.5.8.sif lowcharts hist
